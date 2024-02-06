faec997600ce76a1a84ab9597daa73506bdcf131
TheWavesgod <xtr2811683@gmail.com>
2024-02-06 23:20:48 +0000

    NetworkSteam #5

--- CSC8503/steam.cpp
+++ CSC8503/steam.cpp
@@ -4,6 +4,10 @@
 using namespace NCL;
 using namespace CSC8503;
 
+NetSystem_Steam::NetSystem_Steam()
+{
+}
+
 void NetSystem_Steam::CreateLobby()
 {
 	SteamAPICall_t hCreateLobby = SteamMatchmaking()->CreateLobby(ELobbyType::k_ELobbyTypePublic, 4);
@@ -13,8 +17,28 @@ void NetSystem_Steam::CreateLobby()
 
 void NetSystem_Steam::On_LobbyCreated(LobbyCreated_t* pResult, bool bIOFailure)
 {
-	std::cout << "bIOFailure: " << bIOFailure << std::endl;
-	std::cout << "LobbyCreatedResult: " << pResult->m_eResult << std::endl;
+	std::cout << "LobbyCreatedResult: ";
+	switch (pResult->m_eResult)
+	{
+	case EResult::k_EResultOK:
+		std::cout << "the lobby was successfully created\n";
+		break;
+	case EResult::k_EResultNoConnection:
+		std::cout << "your Steam client doesn't have a connection to the back-end\n";
+		break;
+	case EResult::k_EResultTimeout:
+		std::cout << "you the message to the Steam servers, but it didn't respond\n";
+		break;
+	case EResult::k_EResultFail:
+		std::cout << "the server responded, but with an unknown internal error\n";
+		break;
+	case EResult::k_EResultAccessDenied:
+		std::cout << "your game isn't set to allow lobbies, or your client does haven't rights to play the game\n";
+		break;
+	case EResult::k_EResultLimitExceeded:
+		std::cout << "your game client has created too many lobbies\n";
+		break;
+	}
 	std::cout << "m_ulSteamIDLobby: " << pResult->m_ulSteamIDLobby << std::endl;
 
 	steamIDLobby = pResult->m_ulSteamIDLobby;
@@ -40,6 +64,17 @@ void NetSystem_Steam::On_LobbyMatchList(LobbyMatchList_t* pLobbyMatchList, bool 
 {
 	std::cout << "bIOFailure: " << bIOFailure << std::endl;
 	std::cout << "Number Of Lobbies found: " << pLobbyMatchList->m_nLobbiesMatching << std::endl;
+}
+
+void NetSystem_Steam::JoinLobby()
+{
+	SteamAPICall_t hJoinLobby = SteamMatchmaking()->JoinLobby(steamIDLobby);
+
+}
+
+void NetSystem_Steam::On_LobbyJoined(LobbyEnter_t* pResult, bool bIOFailure)
+{
+
 }
 
 bool NetSystem_Steam::SetLobbyData(ELobbyDataType type, string Value)
--- CSC8503/steam.h
+++ CSC8503/steam.h
@@ -22,21 +22,29 @@ namespace NCL {
 				"GameName"
 			};
 
+			NetSystem_Steam();
+
+		public:
 			void CreateLobby();
 
 			void SearchLobbies();
+
+			void JoinLobby();
 
 			bool SetLobbyData(ELobbyDataType DataType, string Value);
 
 			virtual void On_LobbyCreated(LobbyCreated_t* pResult, bool bIOFailure);
 			virtual void On_LobbyMatchList(LobbyMatchList_t* pLobbyMatchList, bool bIOFailure);
-
+			virtual void On_LobbyJoined(LobbyEnter_t* pResult, bool bIOFailure);
 		protected:
 			string GameName = "ProjetT";
 
 		private:
 			CCallResult<NetSystem_Steam, LobbyCreated_t> m_LobbyCreateCallResult;
 			CCallResult<NetSystem_Steam, LobbyMatchList_t> m_LobbyMatchListCallResult;
+			CCallResult<NetSystem_Steam, LobbyEnter_t> m_JoinLobbyCallResult;
+
+			//CCallback<NetSystem_Steam, LobbyChatUpdate_t> m_LobbyChatUpdateCallback;
 
 			uint64 steamIDLobby = 0;
 		};
