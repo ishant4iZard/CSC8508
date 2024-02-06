b0a03b49ac2c8cf7b6c9881754c58bd2952e5893
TheWavesgod <xtr2811683@gmail.com>
2024-02-06 17:00:05 +0000

    NetWorkingSteam #3

--- CSC8503/steam.cpp
+++ CSC8503/steam.cpp
@@ -16,11 +16,22 @@ void NetSystem_Steam::On_LobbyCreated(LobbyCreated_t* pResult, bool bIOFailure)
 	std::cout << "bIOFailure: " << bIOFailure << std::endl;
 	std::cout << "LobbyCreatedResult: " << pResult->m_eResult << std::endl;
 	std::cout << "m_ulSteamIDLobby: " << pResult->m_ulSteamIDLobby << std::endl;
+
+	steamIDLobby = pResult->m_ulSteamIDLobby;
+	if (SetLobbyData(ELobbyDataType::EGameName, GameName.c_str()))
+	{
+		std::cout << "Set Game Name Success!\n";
+	}
+	
 }
 
 void NetSystem_Steam::SearchLobbies()
 {
-	SteamAPICall_t hRequestLobbyList = SteamMatchmaking()->RequestLobbyList();
+	ISteamMatchmaking* SteamMatchmakingPtr = SteamMatchmaking();
+
+	SteamMatchmakingPtr->AddRequestLobbyListStringFilter(LobbyDataKey[ELobbyDataType::EGameName].c_str(), GameName.c_str(), ELobbyComparison::k_ELobbyComparisonEqual);
+
+	SteamAPICall_t hRequestLobbyList = SteamMatchmakingPtr->RequestLobbyList();
 
 	m_LobbyMatchListCallResult.Set(hRequestLobbyList, this, &NetSystem_Steam::On_LobbyMatchList);
 }
@@ -29,4 +40,11 @@ void NetSystem_Steam::On_LobbyMatchList(LobbyMatchList_t* pLobbyMatchList, bool 
 {
 	std::cout << "bIOFailure: " << bIOFailure << std::endl;
 	std::cout << "Number Of Lobbies found: " << pLobbyMatchList->m_nLobbiesMatching << std::endl;
+}
+
+bool NetSystem_Steam::SetLobbyData(ELobbyDataType type, string Value)
+{
+	if (steamIDLobby == 0) return false;
+
+	return SteamMatchmaking()->SetLobbyData(steamIDLobby, LobbyDataKey[type].c_str(), Value.c_str());
 }
--- CSC8503/steam.h
+++ CSC8503/steam.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <steam_api.h>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
 
 namespace NCL {
 	namespace  CSC8503 {
@@ -8,18 +13,32 @@ namespace NCL {
 		class NetSystem_Steam
 		{
 		public:
+			enum ELobbyDataType {
+				EGameName = 0,
+				ETypeMax
+			};
+
+			vector<string> LobbyDataKey = {
+				"GameName"
+			};
+
 			void CreateLobby();
 
 			void SearchLobbies();
+
+			bool SetLobbyData(ELobbyDataType DataType, string Value);
 
 			virtual void On_LobbyCreated(LobbyCreated_t* pResult, bool bIOFailure);
 			virtual void On_LobbyMatchList(LobbyMatchList_t* pLobbyMatchList, bool bIOFailure);
 
 		protected:
+			string GameName = "ProjetT";
 
 		private:
 			CCallResult<NetSystem_Steam, LobbyCreated_t> m_LobbyCreateCallResult;
 			CCallResult<NetSystem_Steam, LobbyMatchList_t> m_LobbyMatchListCallResult;
+
+			uint64 steamIDLobby = 0;
 		};
 	}
 }
