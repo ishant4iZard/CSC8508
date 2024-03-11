7743f1ce7d44807ecdeda783e35015a28461a078
TheBossBaby47 <144945140+TheBossBaby47@users.noreply.github.com>
2024-03-11 14:56:33 +0000

    Removed build directory from Generate.bat

--- Generate.bat
+++ Generate.bat
@@ -1,2 +1,1 @@
-mkdir Build
-cmake.exe -DCMAKE_TOOLCHAIN_FILE="%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5.cmake" -A Prospero %* -DDEBUG_ASSET_ROOT:STRING=/app0/Assets/ -B ./Build/PS5 -S .
+cmake.exe -DCMAKE_TOOLCHAIN_FILE="%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5.cmake" -A Prospero %* -DDEBUG_ASSET_ROOT:STRING=/app0/Assets/
