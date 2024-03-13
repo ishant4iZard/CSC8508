d7cf57434dd19832b35cfdf174cb5e34f3aeb0aa
ohmydante <58134614+ohmydante@users.noreply.github.com>
2024-03-13 15:41:29 +0000

    update

---
+++ CSC8503/Particles.h
@@ -0,0 +1,5 @@
+/*  Original coding tutorial and assets from: https://learnopengl.com/In-Practice/2D-Game/Particles
+	Coding srtuctures inspired from: https://learnopengl.com/code_viewer.php?code=in-practice/breakout/particle_generator.h
+	Modified by Qixu Feng */
+
+#pragma once
