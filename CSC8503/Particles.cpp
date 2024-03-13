f02845d6235baf22d0b02c9362fe0573891ef8a8
ohmydante <58134614+ohmydante@users.noreply.github.com>
2024-03-13 15:59:15 +0000

    update

--- CSC8503/Particles.cpp
+++ CSC8503/Particles.cpp
@@ -1,3 +1,5 @@
 /*  Original coding tutorial and assets from: https://learnopengl.com/In-Practice/2D-Game/Particles
 	Coding srtuctures inspired from: https://learnopengl.com/code_viewer.php?code=in-practice/breakout/particle_generator
 	Modified by Qixu Feng */
+
+#include "Particles.h"
--- CSC8503/Particles.h
+++ CSC8503/Particles.h
@@ -3,3 +3,33 @@
 	Modified by Qixu Feng */
 
 #pragma once
+#include "OGLRenderer.h"
+#include "OGLShader.h"
+#include "OGLTexture.h"
+#include "OGLMesh.h"
+#include "Event.h"
+#include "GameWorld.h"
+#include "GameObject.h"
+#include "RenderObject.h"
+
+namespace NCL {
+	namespace CSC8503 {
+        class Particle {
+        public:
+            Particle() : position(0.0f, 0.0f), velocity(0.0f, 0.0f), color(1.0f, 1.0f, 1.0f, 1.0f), life(0.0f) {}
+            ~Particle() {};
+
+            void SetPosition(const Vector2& inPosition) { position = inPosition; }
+            Vector2 Getposition() const { return position; }
+
+
+        protected:
+            Vector2 position;
+            Vector2 velocity;
+            Vector4 color;
+
+            float life;
+
+        };
+	}
+}
